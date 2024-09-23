d18c6c2d9cba7f3d3965889fbe980f51188b1a36
Ahmed2000Github <ahmedelrhaouti2000@gmail.com>
2024-09-23 19:33:04 +0100

    v 0.0.4 (add recent works)

--- PortfolioShared/Configurations/AppConfig.cs
+++ PortfolioShared/Configurations/AppConfig.cs
@@ -7,6 +7,7 @@ namespace ChatAppShared.Configurations
         public const string MODEL_IMAGE_BASE_URL = "https://raw.githubusercontent.com/Ahmed2000Github/Data/master/3d/Images/";
         public const string MODEL_BASE_URL = "https://raw.githubusercontent.com/Ahmed2000Github/Data/master/3d/Models/";
         public const string PROJECTS_LIST_BASE_URL = "https://raw.githubusercontent.com/Ahmed2000Github/Data/master/projectsdata.json";
+        public const string RECENT_WORKS_LIST_BASE_URL = "https://raw.githubusercontent.com/Ahmed2000Github/Data/master/worksdata.json";
         public const string MODELS_LIST_BASE_URL = "https://raw.githubusercontent.com/Ahmed2000Github/Data/master/modelsdata.json";
     }
 }
---
+++ PortfolioShared/ViewModels/RecentWorkVM.cs
@@ -0,0 +1,10 @@
+﻿namespace PortfolioShared.ViewModels
+{
+    public class RecentWorkVM
+    {
+        public string Title { get; set; }
+        public string Date { get; set; }
+        public string Link { get; set; }
+        public string Thumbnail { get; set; }
+    }
+}
